a051c9a837ae9b86440853bdf4c5fc24ce8bbf96
yoon3nam <lgmysn2@gmail.com>
2025-03-05 08:53:01 +0900

    chore: 린터 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,8 @@ export default tseslint.config(
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      '@typescript-eslint/ban-ts-comment': 'off'
     }
   },
   eslintConfigPrettier
