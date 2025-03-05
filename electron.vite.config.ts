629911a9aa833f986f5886bea622c97488383c27
yoon3nam <lgmysn2@gmail.com>
2025-03-05 13:22:57 +0900

    chore: svg를 컴포넌트로 활용 할 수 있도록 플러그인 설정

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 // @ts-ignore
 import tailwindcss from '@tailwindcss/vite'
 
@@ -17,6 +18,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react(), tailwindcss()]
+    plugins: [react(), tailwindcss(), svgr()]
   }
 })
