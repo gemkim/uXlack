28d55f60f0bb8a54ad33187d861ec60743ded41c
yoon3nam <lgmysn2@gmail.com>
2025-03-10 16:52:44 +0900

    chore: overlay-kit 설치, provider 적용

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,9 +3,12 @@ import './shared/base.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { OverlayProvider } from 'overlay-kit'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <OverlayProvider>
+      <App />
+    </OverlayProvider>
   </React.StrictMode>
 )
