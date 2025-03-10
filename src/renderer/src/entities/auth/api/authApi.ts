81e73c32ce3f29babf4e888b87fdc0aaf722218d
yoon3nam <lgmysn2@gmail.com>
2025-03-10 09:59:09 +0900

    fix: AxiosError

--- src/renderer/src/entities/auth/api/authApi.ts
+++ src/renderer/src/entities/auth/api/authApi.ts
@@ -1,14 +1,16 @@
 import { fetchApi } from '@renderer/shared/lib/api'
 import { UserDto } from './types'
+import { AxiosError } from 'axios'
 
 export async function createUserAccount(userDto: UserDto) {
   try {
     const res = await fetchApi.post('/user', userDto)
-
     return res
   } catch (error) {
-    if (error.code === 'ERR_NETWORK') {
-      return { status: 400 }
+    if (error instanceof AxiosError) {
+      if (error.code === 'ERR_NETWORK') {
+        return { status: 400 }
+      }
     }
     return { status: 500 }
   }
