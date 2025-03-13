314e79982f28d2be271828bd4781562bdbabe859
yoon3nam <lgmysn2@gmail.com>
2025-03-13 09:34:38 +0900

    fix: 선택 프로젝트 변경 로직

--- src/renderer/src/widget/ChatNavigator/ui/ChatNavigator.tsx
+++ src/renderer/src/widget/ChatNavigator/ui/ChatNavigator.tsx
@@ -1,16 +1,27 @@
+import { useProjectList, useSelectedProjectId } from '@renderer/entities/project'
+
 export function ChatNavigator() {
+  const projectList = useProjectList()
+  const selectedProjectId = useSelectedProjectId()
+
+  const selectedProject = projectList.find((project) => project.id === selectedProjectId)
+
   return (
     <div className="bg-gray-900 p-4 flex flex-col min-w-[180px] text-white">
-      <div>
-        프로젝트 OOO <br />
-        <span className="text-sm">9명</span>
-      </div>
-      <button className="w-full text-left p-2 bg-gray-500">일정</button>
+      {selectedProject && (
+        <>
+          <div>
+            {selectedProject.name} <br />
+            <span className="text-sm">{selectedProject.memberList.length}명</span>
+          </div>
+          <button className="w-full text-left p-2 bg-gray-500">일정</button>
 
-      <div className="mt-4 flex flex-col">
-        <span>채팅</span>
-        <button className="text-left"># 일반</button>
-      </div>
+          <div className="mt-4 flex flex-col">
+            <span>채팅</span>
+            <button className="text-left"># 일반</button>
+          </div>
+        </>
+      )}
     </div>
   )
 }
--- src/renderer/src/widget/MainNavigator/ui/MainNavigator.tsx
+++ src/renderer/src/widget/MainNavigator/ui/MainNavigator.tsx
@@ -1,11 +1,19 @@
 import { useProjectActions, useProjectList, useSelectedProjectId } from '@renderer/entities/project'
+import { CreateNewProjectModal } from '@renderer/features/project/createNewProjectModal/ui/CreateNewProjectModal'
 import { cn } from '@renderer/shared/lib'
 import { ProjectIcon } from '@renderer/shared/ui'
+import { overlay } from 'overlay-kit'
 
 export function MainNavigator() {
   const projectList = useProjectList()
   const selectedProjectId = useSelectedProjectId()
   const { setSelectedProjectId } = useProjectActions()
+
+  function handleNewProjectClick() {
+    overlay.open(({ isOpen, close, unmount }) => (
+      <CreateNewProjectModal isOpen={isOpen} close={close} unmount={unmount} />
+    ))
+  }
 
   function handleProjectClick(id: string) {
     setSelectedProjectId(id)
@@ -14,20 +22,29 @@ export function MainNavigator() {
   const isSelectedProject = (id: string) => id === selectedProjectId
 
   return (
-    <div className="py-2 h-screen bg-black flex flex-col gap-2">
-      {projectList.map((project) => (
-        <div key={project.id} className="relative px-2">
-          {isSelectedProject(project.id) && (
-            <div className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-1/2 size-[8px] bg-blue-200"></div>
-          )}
-          <ProjectIcon
-            className={cn(isSelectedProject(project.id) && '!rounded-2xl')}
-            onClick={() => handleProjectClick(project.id)}
-          >
-            {project.name[0]}
-          </ProjectIcon>
-        </div>
-      ))}
+    <div className="py-4 h-screen bg-black flex flex-col">
+      <button
+        onClick={handleNewProjectClick}
+        className="text-white p-2 border text-sm cursor-pointer "
+      >
+        새 프로젝트
+      </button>
+      <div className="my-4 w-full h-1 border-t border-white"></div>
+      <div className="flex flex-col gap-2">
+        {projectList.map((project) => (
+          <div key={project.id} className="relative px-2 flex justify-center">
+            {isSelectedProject(project.id) && (
+              <div className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-1/2 size-[8px] bg-blue-200"></div>
+            )}
+            <ProjectIcon
+              className={cn(isSelectedProject(project.id) && '!rounded-2xl')}
+              onClick={() => handleProjectClick(project.id)}
+            >
+              {project.name[0]}
+            </ProjectIcon>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
