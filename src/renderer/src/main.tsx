bf86eacdc26e94f8316434005ad7bd848c18101e
yoon3nam <lgmysn2@gmail.com>
2025-03-13 09:33:13 +0900

    chore: strict 제거

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,17 +1,14 @@
 import './shared/base.css'
 
-import React from 'react'
+import { OverlayProvider } from 'overlay-kit'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { OverlayProvider } from 'overlay-kit'
 import AuthDataProvider from './app/provider/AuthDataProvider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <AuthDataProvider>
-      <OverlayProvider>
-        <App />
-      </OverlayProvider>
-    </AuthDataProvider>
-  </React.StrictMode>
+  <AuthDataProvider>
+    <OverlayProvider>
+      <App />
+    </OverlayProvider>
+  </AuthDataProvider>
 )
