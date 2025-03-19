9d14a879d4feb018276bbfb9fd9bf2fbdb6f060b
yoon3nam <lgmysn2@gmail.com>
2025-03-19 15:02:16 +0900

    fix: 트리거 위치별 popover margin 조절, 모션 조절

--- src/renderer/src/shared/ui/Popover/Popover.tsx
+++ src/renderer/src/shared/ui/Popover/Popover.tsx
@@ -1,42 +1,78 @@
 import useOnOutsideClick from '@renderer/shared/hooks/useOnOutsideClick'
 import { cn } from '@renderer/shared/lib'
 import { OverlayProps } from '@renderer/shared/types/overlayProps'
+import { AnimatePresence, Variants, motion } from 'motion/react'
 
 interface PopoverProps extends OverlayProps {
   triggerRect: DOMRect
 }
 
+const popoverVariants: Variants = {
+  initial: (option) => ({
+    opacity: 0,
+    translateX: option.isToLeft ? 30 : -30
+  }),
+  animate: (option) => ({
+    opacity: 1,
+    translateX: 0,
+    translateY: option.isToTop ? '-100%' : 0,
+    transition: {
+      translateY: { duration: 0 }
+    }
+  }),
+  exit: (option) => ({
+    opacity: 0,
+    translateX: option.isToLeft ? 30 : -30
+  })
+}
+
 export function Popover(props: PopoverProps) {
-  const { isOpen, close, unmount, triggerRect } = props
+  const { isOpen, close, unmount, triggerRect, children } = props
   const { x, y } = triggerRect
 
   const ref = useOnOutsideClick(() => {
+    close()
     if (unmount) {
-      close()
-      if (unmount) {
-        setTimeout(() => {
-          unmount()
-        }, 1000)
-      }
+      setTimeout(() => {
+        unmount()
+      }, 600)
     }
   })
 
-  function handleContentClick(event: React.MouseEvent<HTMLDivElement>) {
-    // event.stopPropagation()
-    console.log('콘텐츠')
+  const isToLeft = x > window.innerWidth / 2
+  const isToTop = y > window.innerHeight / 2
+  const gap = 20
+
+  const margins = {
+    marginTop: `${y}px`,
+    marginBottom: `${window.innerHeight - y}px`,
+    marginLeft: isToLeft ? 'auto' : `${x + gap}px`,
+    marginRight: isToLeft ? `${window.innerWidth - x + gap}px` : 'auto'
   }
+
+  console.log(isToTop)
   return (
-    <div className="fixed left-0 top-0 w-screen h-screen z-[90] pointer-events-none">
+    <div className="fixed left-0 top-0 w-screen h-screen z-[90] pointer-events-none ">
       {/* popover */}
-      <div
-        ref={ref}
-        className={cn(
-          'size-[300px] bg-white pointer-events-auto shadow-md border',
-          !isOpen && 'opacity-0 pointer-events-none'
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            transition={{ type: 'spring', duration: 0.2, stiffness: 200, damping: 20 }}
+            variants={popoverVariants}
+            ref={ref}
+            custom={{ isToLeft, isToTop }}
+            initial="initial"
+            animate="animate"
+            exit="exit" // ✅ exit 애니메이션 실행
+            className={cn(
+              'p-4 bg-white rounded-md max-w-[50%] max-h-[50%] pointer-events-auto shadow-md border transition-opacity'
+            )}
+            style={margins}
+          >
+            {children}
+          </motion.div>
         )}
-        onClick={handleContentClick}
-        style={{ marginLeft: `${x + 30}px`, marginTop: `${y}px` }}
-      ></div>
+      </AnimatePresence>
     </div>
   )
 }
