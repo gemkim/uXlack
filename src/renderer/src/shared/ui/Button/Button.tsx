a7654ce0dbb9da33836233d4fbc2c9241bc6d298
yoon3nam <lgmysn2@gmail.com>
2025-03-25 16:10:28 +0900

    fix(style): Button colorScheme 추가

--- src/renderer/src/shared/ui/Button/Button.tsx
+++ src/renderer/src/shared/ui/Button/Button.tsx
@@ -6,17 +6,33 @@ interface ButtonProps
     HTMLButtonElement
   > {
   isActive?: boolean
+  colorScheme?: 'gray' | 'blue'
+}
+
+/**
+ * 각 color scheme마다 hover: 반드시 필요
+ *
+ * 없으면 highlightColorClassName 에러 발생
+ */
+const buttonColorStyles = {
+  gray: 'hover:bg-neutral-200',
+  blue: 'bg-blue-550 hover:bg-blue-650 text-white'
 }
 
 export function Button(props: ButtonProps) {
-  const { className, children, isActive, ...attrs } = props
+  const { className, children, isActive, colorScheme = 'gray', ...attrs } = props
+
+  const highlightColorClassName = buttonColorStyles[colorScheme]
+    .match(/\bhover:[\w-]+\b/)![0]
+    .replace(/^hover:/, '')
 
   return (
     <button
       className={cn(
         className,
-        'flex items-center transition-colors gap-2 py-1 px-1.5 rounded-sm hover:bg-neutral-200',
-        isActive && 'bg-neutral-200'
+        'flex items-center transition-colors gap-2 py-1 px-1.5 rounded-sm',
+        buttonColorStyles[colorScheme],
+        isActive && highlightColorClassName
       )}
       {...attrs}
     >
