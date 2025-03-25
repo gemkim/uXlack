1b78f7a19b6ad9f426242593d879879544954dce
yoon3nam <lgmysn2@gmail.com>
2025-03-25 09:38:01 +0900

    fix(style): Button 컴포넌트 isAcitve props 추가

--- src/renderer/src/shared/ui/Button/Button.tsx
+++ src/renderer/src/shared/ui/Button/Button.tsx
@@ -4,16 +4,19 @@ interface ButtonProps
   extends React.DetailedHTMLProps<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
-  > {}
+  > {
+  isActive?: boolean
+}
 
 export function Button(props: ButtonProps) {
-  const { className, children, ...attrs } = props
+  const { className, children, isActive, ...attrs } = props
 
   return (
     <button
       className={cn(
         className,
-        'flex items-center transition-colors gap-2 py-1 px-1.5 rounded-sm hover:bg-neutral-200'
+        'flex items-center transition-colors gap-2 py-1 px-1.5 rounded-sm hover:bg-neutral-200',
+        isActive && 'bg-neutral-200'
       )}
       {...attrs}
     >
