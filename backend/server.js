dda10451756d40db172fdaac4afedb90cf73616d
yoon3nam <lgmysn2@gmail.com>
2025-03-26 16:26:20 +0900

    (b)refactor: socket 분리

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import express from 'express'
 import http from 'http'
 import mongoose from 'mongoose'
 import { Server } from 'socket.io'
-import Message from './models/message.js'
+
+import setupMessageSocket from './sockets/message.socket.js'
 
 const app = express()
 const server = http.createServer(app)
@@ -24,45 +25,8 @@ mongoose
   .then(() => console.log('mongo connect'))
   .catch((err) => console.log(err))
 
-// 로그인시 최초 소켓 세션 연결
-io.on('connection', (socket) => {
-  const userId = socket.handshake.query.userId
-  console.log('새 사용자 연결:', socket.id, userId)
-
-  // 프로젝트(채팅) 입장
-  socket.on('join-rooms', (projectIdList) => {
-    projectIdList.forEach((projectId) => {
-      socket.join(projectId)
-      console.log(socket.id, '방참여', projectId)
-    })
-  })
-
-  socket.on('send-message', async (projectId, msgDto) => {
-    const newMessage = new Message({
-      ...msgDto
-    })
-    await newMessage.save()
-
-    io.to(projectId).emit('receive-message', msgDto)
-  })
-
-  socket.on('get-all-projects-message-list', async (projectId, callback) => {
-    const messages = await Message.find({ projectId }).sort({ createdAt: 1 })
-
-    // projectIdList.forEach(async (id) => {
-    //   const messages = await Message.find({ projectId: id }).sort({ createdAt: 1 })
-    //   messageOfProject.id = messages
-    // })
-    callback(messages)
-  })
-  // socket.on('user_join', (username) => {
-  //   users[socket.id] = username
-
-  //   console.log('userId :', username)
-
-  //   io.emit('user_joined', { username, users: Object.values(users) })
-  // })
-})
+// 소켓 연동
+setupMessageSocket(io)
 
 server.listen(PORT, () => {
   console.log(`채팅 서버가 ${PORT} 포트에서 실행 중입니다`)
---
+++ backend/sockets/message.socket.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import Message from '../models/message.js'
+
+const setupMessageSocket = (io) => {
+  io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId
+    console.log('새 사용자 연결:', socket.id, userId)
+
+    // 프로젝트(채팅) 입장
+    socket.on('join-rooms', (projectIdList) => {
+      projectIdList.forEach((projectId) => {
+        socket.join(projectId)
+        console.log(socket.id, '방참여', projectId)
+      })
+    })
+
+    socket.on('send-message', async (projectId, msgDto) => {
+      const newMessage = new Message({
+        ...msgDto
+      })
+      await newMessage.save()
+
+      io.to(projectId).emit('receive-message', msgDto)
+    })
+
+    socket.on('get-all-projects-message-list', async (projectId, callback) => {
+      const messages = await Message.find({ projectId }).sort({ createdAt: 1 })
+
+      // projectIdList.forEach(async (id) => {
+      //   const messages = await Message.find({ projectId: id }).sort({ createdAt: 1 })
+      //   messageOfProject.id = messages
+      // })
+      callback(messages)
+    })
+    // socket.on('user_join', (username) => {
+    //   users[socket.id] = username
+
+    //   console.log('userId :', username)
+
+    //   io.emit('user_joined', { username, users: Object.values(users) })
+    // })
+  })
+}
+
+export default setupMessageSocket
