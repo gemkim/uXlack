86c9e9609d2611dad01b21381f01761df0d90bb5
yoon3n4m <lgmysn2@gmail.com>
2025-04-16 08:46:53 +0900

    (b)feat: project, profile 라우터 api 명세 작성

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -3,7 +3,59 @@ import { updateProfileIconSeed, updateProfileName } from '../controllers/profile
 
 const profileRouter = Router()
 
-profileRouter.put('/update-icon', updateProfileIconSeed)
+/**
+ * @swagger
+ * /profile/update-name:
+ *   put:
+ *     summary: profile의 name 변경 api
+ *     description: api 생성만 해두고 테스트를 해보지 못했습니다. 근데 될 거 같음
+ *     tags: [Profile]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: 프로필 이름 변경 완료
+ *       401:
+ *         description: 로그인이 필요합니다.
+ *       404:
+ *         description: 프로필을 찾을 수 없습니다.
+ *       500:
+ *         description: 그 외 서버 에러
+ */
 profileRouter.put('/update-name', updateProfileName)
+
+/**
+ * @swagger
+ * /profile/update-icon:
+ *   put:
+ *     summary: profile의 iconSeed 변경 api
+ *     tags: [Profile]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               iconSeed:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: 프로필 아이콘 시드 변경 완료
+ *       401:
+ *         description: 로그인이 필요합니다.
+ *       404:
+ *         description: 프로필을 찾을 수 없습니다.
+ *       500:
+ *         description: 그 외 서버 에러
+ */
+profileRouter.put('/update-icon', updateProfileIconSeed)
 
 export default profileRouter
