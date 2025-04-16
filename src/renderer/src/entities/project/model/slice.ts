768b4987915e684c912c3550e936ec8cd8586de7
yoon3n4m <lgmysn2@gmail.com>
2025-04-16 16:03:17 +0900

    feat: 첫 로그인시 본인 소속 프로젝트의 모든 사용자 Profile 조회

--- src/renderer/src/entities/project/model/slice.ts
+++ src/renderer/src/entities/project/model/slice.ts
@@ -1,15 +1,18 @@
 import { create } from 'zustand'
 import { ProjectDto } from '../types/types'
+import { ProfileDto } from '@renderer/entities/auth/types'
 
 interface ProjectStore {
   projectList: ProjectDto[]
   selectedProjectId: string | null
   selectedProject: ProjectDto | null
+  profileList: ProfileDto[]
   actions: {
     addProject: (project: ProjectDto) => void
     removeProject: (projectId: string) => void
     setProjectList: (projectList: ProjectDto[]) => void
     setSelectedProjectId: (projectId: string | null) => void
+    setProfileList: (profileList: ProfileDto[]) => void
   }
 }
 
@@ -17,6 +20,7 @@ const useProjectStore = create<ProjectStore>((set) => ({
   projectList: [],
   selectedProjectId: null,
   selectedProject: null,
+  profileList: [],
   actions: {
     addProject: (project) => set((state) => ({ projectList: [...state.projectList, project] })),
     removeProject: (projectId) =>
@@ -28,11 +32,13 @@ const useProjectStore = create<ProjectStore>((set) => ({
       set((state) => ({
         selectedProjectId: projectId,
         selectedProject: state.projectList.find((item) => item._id === projectId)
-      }))
+      })),
+    setProfileList: (profileList) => set({ profileList })
   }
 }))
 
 export const useProjectList = () => useProjectStore((state) => state.projectList)
+export const useProfileList = () => useProjectStore((state) => state.profileList)
 export const useSelectedProjectId = () => useProjectStore((state) => state.selectedProjectId)
 export const useSelectedProject = () => useProjectStore((state) => state.selectedProject)
 export const useProjectActions = () => useProjectStore((state) => state.actions)
