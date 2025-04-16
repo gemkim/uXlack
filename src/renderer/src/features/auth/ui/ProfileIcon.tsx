5d32fef49ce8b0a85f9117b8d4270698b51c373c
yoon3n4m <lgmysn2@gmail.com>
2025-04-16 14:20:07 +0900

    clean: console.log 제거

--- src/renderer/src/features/auth/ui/ProfileIcon.tsx
+++ src/renderer/src/features/auth/ui/ProfileIcon.tsx
@@ -12,14 +12,10 @@ export default function ProfileIcon(props: ProfileIconProps) {
   const { className, seed: propsSeed } = props
   const profile = useProfile()
 
-  console.log(profile)
-
   if (!profile) return
 
   const seed = propsSeed ?? profile.iconSeed ?? profile.name
   const avatarSvgStr = createAvatar(thumbs, { seed: seed, scale: 70 }).toString()
-
-  console.log(seed)
 
   return (
     <div
