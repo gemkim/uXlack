b2497a2693c2f8a2da4e065b816cffd4800236c6
yoon3n4m <lgmysn2@gmail.com>
2025-05-13 13:38:12 +0900

    feat: task Dto (type)

--- src/renderer/src/entities/task/types/index.ts
+++ src/renderer/src/entities/task/types/index.ts
@@ -1,3 +1,16 @@
 export interface TaskDto {
   projectId: string
+  name: string
+  desc: string
+  startDate: Date
+  endDate?: Date
+  // tag도 있어야할듯한데
+  tagId: string
+  // 일정 관련자들의 profileId 배열
+  assignee?: string[]
+
+  // mongoose 자동 생성 필드
+  _id?: string
+  createAt?: string
+  updatedAt?: string
 }
