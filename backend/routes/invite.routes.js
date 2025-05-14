5f2db406111b3fb2bf39b4eed0c72f08cfc27c95
yoon3n4m <lgmysn2@gmail.com>
2025-05-14 10:05:24 +0900

    chore: backend 레포 분리

--- backend/routes/invite.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getReceivedInvites, sendInvite } from '../controllers/invite.controller.js'
-
-const inviteRouter = Router()
-
-inviteRouter.post('/send', sendInvite)
-inviteRouter.post('/get-received', getReceivedInvites)
-
-export default inviteRouter
